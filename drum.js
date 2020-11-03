d1b86f105ac668ce073199e6100d2ba77bb64717
SudhapavaniCh <sudhapavanichowdary95@gmail.com>
2020-11-03 16:31:22 -0600

    Final version of the project

---
+++ drum.js
@@ -0,0 +1,56 @@
+var numberDrum=document.querySelectorAll(".drum").length;
+var i=0;
+while(i < numberDrum)
+ {
+	document.querySelectorAll(".drum")[i].addEventListener("click",handleClicks);
+	function handleClicks()
+	{
+
+		var buttonInnerHTML=this.innerHTML;
+
+
+		switch (buttonInnerHTML){
+		case "w":
+		var audio = new Audio('tom-1.mp3');
+	    audio.play();
+	    break;
+
+	    case "a":
+		var audio = new Audio('tom-2.mp3');
+	    audio.play();
+	    break;
+
+	    case "s":
+		var audio = new Audio('tom-3.mp3');
+	    audio.play();
+	    break;
+
+	    case "d":
+		var audio = new Audio('tom-4.mp3');
+	    audio.play();
+	    break;
+
+	     case "j":
+		var audio = new Audio('kick-bass.mp3');
+	    audio.play();
+	    break;
+
+	     case "k":
+		var audio = new Audio('snare.mp3');
+	    audio.play();
+	    break;
+
+	     case "l":
+		var audio = new Audio('crash.mp3');
+	    audio.play();
+	    break;
+	     default:
+	    console.log(buttonInnerHTML);
+		}
+	
+	};
+	i++;
+ }
+
+
+
